db3829292eab712eec2be612fac4700057595130
Kai Mihata <kaimihata@D-10-19-32-131.dhcp4.washington.edu>
2018-10-31 18:58:47 -0700

    Added a keyword search for specific items on the site

--- checkout.py
+++ checkout.py
@@ -1,9 +1,14 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# Initializes the chromewebdriver
 driver = webdriver.Chrome(executable_path=r"/Users/kaimihata/Desktop/CheckoutBot/chromedriver")
-driver.get("https://www.supremenewyork.com/shop/all/pants")
-items = driver.find_elements_by_xpath(".//a[contains(text(), 'Champion')]")
+keyword = "Crew"
+usr_color = "Black"
+category = "accessories"
+driver.get("https://www.supremenewyork.com/shop/all/" + category)
+items = driver.find_elements_by_xpath(".//a[contains(text(), " + "'" + keyword + "'" + ")]")
 colors = []
 for item in items:
     colors.append(item.get_attribute("href"))
@@ -11,18 +16,37 @@ for item in items:
 item_paths = []
 for color in colors:
     item_paths.append(driver.find_elements_by_xpath(".//a[contains(@href, " + "'" + color[30:] + "'" + ")]"))
- 
-for item_path in item_paths:
-    print(item_path[2].get_attribute("text") + ": " + item_path[2].get_attribute("href")[30:])
 
-driver.get(item_paths[0][2].get_attribute("href"))
-#s_code = driver.find_element_by_xpath("//option[text() = 'Small']").get_attribute("value")
-#m_code = driver.find_element_by_xpath("//option[text() = 'Medium']").get_attribute("value")
-l_code = driver.find_element_by_xpath("//option[text() = 'Large']").get_attribute("value")
-xl_code = driver.find_element_by_xpath("//option[text() = 'XLarge']").get_attribute("value")
+path = ""
+for item_path in item_paths:
+    if usr_color in item_path[2].get_attribute("text"):
+        path = item_path[2].get_attribute("href")
+if (path == ""):
+    path = item_paths[0][2].get_attribute("href")
+
+print("https://www.supremenewyork.com" + path)
+driver.get(path)
+
+size_codes = []
+try:
+    size_codes.append("S " + driver.find_element_by_xpath("//option[text() = 'Small']").get_attribute("value"))
+except:
+    e = sys.exc_info()[0]
+try:
+    size_codes.append("M " + driver.find_element_by_xpath("//option[text() = 'Medium']").get_attribute("value"))
+except:
+    e = sys.exc_info()[0]
+try:
+    size_codes.append("L " + driver.find_element_by_xpath("//option[text() = 'Large']").get_attribute("value"))
+except:
+    e = sys.exc_info()[0]
+try:
+    size_codes.append("X " + driver.find_element_by_xpath("//option[text() = 'XLarge']").get_attribute("value"))
+except:
+    e = sys.exc_info()[0]
+
 token = driver.find_element_by_xpath("//meta[@name ='csrf-token']").get_attribute("content")
 code = driver.find_element_by_xpath("//form[@id ='cart-addf']").get_attribute("action")
-print("Large: " +  l_code)
-print("XLarge: " + xl_code)
+print(size_codes)
 print("Token: " + token)
 print("Code: " + code)
